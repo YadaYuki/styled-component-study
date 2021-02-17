01a68cdaab7da8105a161031b59924c6936bd73c
YadaYuki <yada.yuki@fuji.waseda.jp>
2021-02-17 18:57:58 +0900

    practice styled-component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import styled, { keyframes } from 'styled-components'
+
+const Continer = styled.div`
+text-align: center;
+`
+const Header = styled.header`
+background-color: #282c34;
+min-height: 100vh;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+font-size: calc(10px + 2vmin);
+color: white;
+`
+
+const AppLogoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+
+`
+
+
+const StyledLogo = styled.img`
+height: 40vmin;
+pointer-events: none;
+animation: ${AppLogoSpin} infinite 20s linear;
+`
+
+// const Img = () => {
+//   return <img src={logo} alt="logo" />
+// }
+const AppLink = styled.a``
+
+const StyledAppLink = styled(AppLink)`color: #fff;`
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Continer>
+      <Header>
+        <StyledLogo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+        <StyledAppLink href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          rel="noopener noreferrer" >Learn React</StyledAppLink>
+      </Header>
+    </Continer>
   );
 }
 
